b6a844fad2419ee9b50c248afceddab6b4b87960
nightlinks-nyc <nightlinks.nyc@gmail.com>
2025-03-05 01:31:01 -0500

    Update makeHead.js

--- js/makeHead.js
+++ js/makeHead.js
@@ -1,8 +1,8 @@
 function makeHead(pageName) {	
-	let headCode =                                                                                            '\n' + 
-    '<head>'                                                                                                     + '\n' + 
-    '<title>Nightlinks | ' + pageName + '</title>'                                                               + '\n' + 
-    '<link rel="icon" href="https://katcottrell.github.io/vampire-interviews/images/icons/Nightlink%20-%20solid.png">' + '\n' + 
-    '</head>'                                                                                                    + '\n';
+	let headCode =                                                               '\n' + 
+    '<head>'                                                                       + '\n' + 
+    '<title>Nightlinks | ' + pageName + '</title>'                                 + '\n' + 
+    '<link rel="icon" href="nightlinks-nyc.com/images/icons/nightlinks_icon.png">' + '\n' + 
+    '</head>'                                                                      + '\n';
   document.write(headCode);
 }
