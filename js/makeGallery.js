7717af8af3f741560c274ee099d65b253d0bbc95
nightlinks-nyc <nightlinks.nyc@gmail.com>
2025-03-05 03:06:58 -0500

    fixed paths

--- js/makeGallery.js
+++ js/makeGallery.js
@@ -1,27 +1,27 @@
 let absGallery = 'https://nightlinks-nyc.com/';
 
 // GALLERIES
-const photos_wanted = ["photos_wanted.jpg", "https://nightlinks-nyc.com/about.html#contact",
-                       "photos_wanted.jpg", "https://nightlinks-nyc.com/about.html#contact",
-                       "photos_wanted.jpg", "https://nightlinks-nyc.com/about.html#contact"]
-const homeCategories = ["home/cosmetology.JPG",       "cosmetology.html",
-                        "home/events-and-venues.JPG", "events-and-venues.html",
-                        "home/merchants.JPG",         "merchants.html",
-                        "home/music-and-audio.JPG",   "music-and-audio.html",
-                        "home/occult.JPG",            "occult.html",
-                        "home/performing-arts.JPG",   "performing-arts.html",
-                        "home/photo-and-video.JPG",   "photo-and-video.html",
-                        "home/production.JPG",        "production.html",
-                        "home/stem.JPG",              "stem.html",
-                        "home/visual-arts.JPG",       "visual-arts.html",
-                        "home/writing.JPG",           "writing.html",
-                        "home/other.JPG",             "other.html"]
+const photos_wanted = ["photos_wanted.jpg",           "about.html#contact",
+                       "photos_wanted.jpg",           "about.html#contact",
+                       "photos_wanted.jpg",           "about.html#contact"]
+const homeCategories = ["home/cosmetology.JPG",       "categories/cosmetology.html",
+                        "home/events-and-venues.JPG", "categories/events-and-venues.html",
+                        "home/merchants.JPG",         "categories/merchants.html",
+                        "home/music-and-audio.JPG",   "categories/music-and-audio.html",
+                        "home/occult.JPG",            "categories/occult.html",
+                        "home/performing-arts.JPG",   "categories/performing-arts.html",
+                        "home/photo-and-video.JPG",   "categories/photo-and-video.html",
+                        "home/production.JPG",        "categories/production.html",
+                        "home/stem.JPG",              "categories/stem.html",
+                        "home/visual-arts.JPG",       "categories/visual-arts.html",
+                        "home/writing.JPG",           "categories/writing.html",
+                        "home/other.JPG",             "categories/other.html"]
 
 function makeGallery(galList) {
     let galleryCode = '\n';
     for (let fileNameIdx = 0; fileNameIdx < galList.length; fileNameIdx = fileNameIdx + 2) {
       let linkIdx = fileNameIdx + 1;
-      galleryCode += '<a href="' + absGallery + 'categories/' + galList[linkIdx] + '">';
+      galleryCode += '<a href="' + absGallery + galList[linkIdx] + '">';
       galleryCode += '<img class="thirds" src="' + absGallery + 'images/' + galList[fileNameIdx] + '">';
       galleryCode += '</a>';
     }
